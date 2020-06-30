c18a7c5201379eee8fca7482f1ab14096a1085e5
Nicolas <nicolasgarciacomp@gmail.com>
2020-06-30 18:37:01 -0300

    Primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ProductosComponent } from "./components/productos/productos.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'inicio', component: InicioComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'productos', component: ProductosComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'inicio' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule{}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    InicioComponent,
+    ProductosComponent,
+    ContactoComponent,
+    NosotrosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
